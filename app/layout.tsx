9128b65d426d44b037a97b061a34a227a1c67081
Shahid Alam <103202284+azhashmi@users.noreply.github.com>
2025-12-13 10:38:27 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export const metadata: Metadata = {
         url: "/og-image.jpg",
         width: 1200,
         height: 630,
-        alt: "The Dime Technology - Freelance Tech Partner",
+        alt: "The Dime Technology - Your Freelance Tech Partner",
       },
     ],
   },
